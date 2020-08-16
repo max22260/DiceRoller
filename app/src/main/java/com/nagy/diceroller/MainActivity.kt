533a6f34fe7ab261d8d27b67337502f2d77f5790
ahmed nagy <ahmedsurviver@gmail.com>
2020-08-16 15:34:21 +0200

    create rolldice Random Genrator

--- app/src/main/java/com/nagy/diceroller/MainActivity.kt
+++ app/src/main/java/com/nagy/diceroller/MainActivity.kt
@@ -1,22 +1,35 @@
-   package com.nagy.diceroller
+package com.nagy.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
-   class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
-        var RollButton : Button = findViewById(R.id.roll_button)
+
+        val RollButton: Button = findViewById(R.id.roll_button)
         RollButton.setOnClickListener {
-            Toast. makeText( this,"lest's Roll",Toast.LENGTH_SHORT).show()
+            RollDice()
+
         }
 
+
+    }
+
+    private fun RollDice() {
+
+        val ResultText: TextView = findViewById(R.id.result_text)
+
+        val RandomInt = Random().nextInt(6) + 1
+
+        ResultText.text = RandomInt.toString()
 
     }
 }
