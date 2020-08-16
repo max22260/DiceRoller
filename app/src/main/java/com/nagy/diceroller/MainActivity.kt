509157d4d7f889f851420729c25a245ff48b6aec
ahmed nagy <ahmedsurviver@gmail.com>
2020-08-16 15:17:43 +0200

    Desplay Tost

--- app/src/main/java/com/nagy/diceroller/MainActivity.kt
+++ app/src/main/java/com/nagy/diceroller/MainActivity.kt
@@ -2,10 +2,21 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+   class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //
+        var RollButton : Button = findViewById(R.id.roll_button)
+        RollButton.setOnClickListener {
+            Toast. makeText( this,"lest's Roll",Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
